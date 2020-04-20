0f07711f929429be82e107910e4e11e7a0ec0ed6
DavlatYldashev <61652029+DavlatYldashev@users.noreply.github.com>
2020-04-20 13:53:55 +0300

    sisisioso

--- supersait/js/common.js
+++ supersait/js/common.js
@@ -1,22 +1,45 @@
 $(document).ready(function() {
-	$('.slider').slick({
-		arrows:true,
-		dots:true,	
-	});
-	$('.slider__section7').slick({
-		arrows:true,
-		dots:false,	
-		slidesToShow:3,
-		slidesToScroll:1,
-		speed:1000,
-	});
-	$('.slider__photo').slick({
-		arrows:false,
-		dots:true,	
-		slidesToShow:1,
-		slidesToScroll:1,
-		speed:1000,
-	});
+    $('.slider').slick({
+        arrows:true,
+        dots:true,  
+        slidesToShow:1,
+    });
+    $('.slider__section7').slick({
+        arrows:true,
+        dots:false, 
+        slidesToShow:3,
+        slidesToScroll:1,
+        speed:1000,
+        adaptiveHeight:true,
+        responsive:[
+            {
+                breakpoint: 910,
+                settings: {
+                    slidesToShow:2,
+                    slidesToScroll:1,
+                }
+            },{
+                breakpoint: 620,
+                settings: {
+                    slidesToShow:1,
+                    slidesToScroll:1,
+                }
+            },{
+                breakpoint: 335,
+                settings: {
+                    slidesToShow:2,
+                    slidesToScroll:1,
+                }
+            }
+        ]
+    });
+    $('.slider__photo').slick({
+        arrows:false,
+        dots:true,  
+        slidesToShow:1,
+        slidesToScroll:1,
+        speed:1000,
+    });
 });
 
 
@@ -70,4 +93,55 @@ $(document).on('click','.box', function(){
     $(this).addClass('active').siblings().removeClass('active')
 });
 
+$('.open-button').click(function() 
+{
+        if(!$('.menu-button').hasClass('open-done'))
+        {
+            $('.menu-button').addClass('open-done');
+            $('.menu-block-1').addClass('open-done');
+            setTimeout(function(){$('.menu-block-2').addClass('open-done')}, 100);
+            setTimeout(function(){$('.menu-link-1').addClass('open-done')}, 200);
+            setTimeout(function(){$('.menu-link-2').addClass('open-done')}, 400);
+            setTimeout(function(){$('.menu-link-3').addClass('open-done')}, 500);
+            setTimeout(function(){$('.menu-link-4').addClass('open-done')}, 600);
+            setTimeout(function(){$('.menu-link-5').addClass('open-done')}, 700);
+            setTimeout(function(){$('.menu-link-6').addClass('open-done')}, 800);
+            setTimeout(function(){$('.menu-link-7').addClass('open-done')}, 900);
+            setTimeout(function(){$('.menu-link-8').addClass('open-done')}, 900);
+        }
+        else
+        {
+            $('.menu-button').removeClass('open-done');
+            $('.menu-link-1').removeClass('open-done');
+            setTimeout(function(){$('.menu-link-1').removeClass('open-done')}, 200);
+            setTimeout(function(){$('.menu-link-2').removeClass('open-done')}, 250);
+            setTimeout(function(){$('.menu-link-3').removeClass('open-done')}, 300);
+            setTimeout(function(){$('.menu-link-4').removeClass('open-done')}, 350);
+            setTimeout(function(){$('.menu-link-5').removeClass('open-done')}, 400);
+            setTimeout(function(){$('.menu-link-6').removeClass('open-done')}, 450);
+            setTimeout(function(){$('.menu-link-7').removeClass('open-done')}, 500);
+            setTimeout(function(){$('.menu-link-8').removeClass('open-done')}, 550);
+            setTimeout(function(){$('.menu-block-1').removeClass('open-done')}, 800);
+            setTimeout(function(){$('.menu-block-2').removeClass('open-done')}, 600);
+        }
+});
+
+    const anchors = document.querySelectorAll('a[href*="#"]')
+
+    for (let anchor of anchors) {
+        anchor.addEventListener("click", function(event){
+            event.preventDefault();
+            const blockID = anchor.getAttribute('href')
+            document.querySelector('' + blockID).scrollIntoView({
+                behavior:"smooth",
+                block:"start"
+            })
+          })
+        }
+
+        $(function (){
+
+            AOS.init();
+
+    }); 
 

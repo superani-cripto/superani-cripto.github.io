2715a9cea66da07deaa2f46379fc0a5ead360e47
DavlatYldashev <61652029+DavlatYldashev@users.noreply.github.com>
2020-04-25 15:08:13 +0300

    DAVLAT

---
+++ DavlatYldashev/js/common.js
@@ -0,0 +1,54 @@
+$('.open-button').click(function() 
+{
+        if(!$('.menu-button').hasClass('open-done'))
+        {
+            $('.menu-button').addClass('open-done');
+            $('.menu-block-1').addClass('open-done');
+            setTimeout(function(){$('.menu-block-2').addClass('open-done')}, 100);
+            setTimeout(function(){$('.menu-link-1').addClass('open-done')}, 500);
+            setTimeout(function(){$('.menu-link-2').addClass('open-done')}, 600);
+            setTimeout(function(){$('.menu-link-3').addClass('open-done')}, 700);
+            setTimeout(function(){$('.menu-link-4').addClass('open-done')}, 800);
+            setTimeout(function(){$('.menu-link-5').addClass('open-done')}, 900);
+        }
+        else
+        {
+            $('.menu-button').removeClass('open-done');
+            $('.menu-link-1').removeClass('open-done');
+            setTimeout(function(){$('.menu-link-1').removeClass('open-done')}, 300);
+            setTimeout(function(){$('.menu-link-2').removeClass('open-done')}, 300);
+            setTimeout(function(){$('.menu-link-3').removeClass('open-done')}, 400);
+            setTimeout(function(){$('.menu-link-4').removeClass('open-done')}, 500);
+            setTimeout(function(){$('.menu-link-5').removeClass('open-done')}, 550);
+            setTimeout(function(){$('.menu-block-1').removeClass('open-done')}, 800);
+            setTimeout(function(){$('.menu-block-2').removeClass('open-done')}, 600);
+        }
+});
+
+    const anchors = document.querySelectorAll('a[href*="#"]')
+
+    for (let anchor of anchors) {
+        anchor.addEventListener("click", function(event){
+            event.preventDefault();
+            const blockID = anchor.getAttribute('href')
+            document.querySelector('' + blockID).scrollIntoView({
+                behavior:"smooth",
+                block:"start"
+            })
+          })
+        }
+
+         $(function (){
+
+            AOS.init();
+
+    }); 
+
+$(document).ready(function(){
+    $(window).scroll(function(){
+        if($(window).scrollTop()>'#header'){
+            $('#skils').fadeIn(900)
+        }else{
+        }
+    });
+});

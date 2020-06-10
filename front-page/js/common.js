c75204d23a6a8909fa79e5b9e482a4e4c7992bce
DavlatYldashev <61652029+DavlatYldashev@users.noreply.github.com>
2020-06-10 22:35:57 +0300

    ddddddddddd

---
+++ front-page/js/common.js
@@ -0,0 +1,31 @@
+$(function () {
+  const anchors = document.querySelectorAll('a[href*="#"]');
+
+  $(document).ready(function() {
+  $('.slider').slick({
+    arrows:false,
+    dots:true,  
+    slidesToShow:1,
+  });
+  $('.slider__section5').slick({
+    arrows:false,
+    dots:true,  
+    slidesToShow:6,
+  });
+});
+
+  for (let anchor of anchors) {
+    anchor.addEventListener("click", function (event) {
+      event.preventDefault();
+      const blockID = anchor.getAttribute("href");
+    });
+  }
+
+});
+
+$(document).on("click", ".box", function () {
+  $(this).addClass("active").siblings().removeClass("active");
+});
+
+
+

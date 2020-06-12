bb4b739df23e07cb3c9cd39e0d210a132f7951d6
DavlatYldashev <61652029+DavlatYldashev@users.noreply.github.com>
2020-06-12 06:43:28 +0300

    vudgnegiu

--- front-page/js/common.js
+++ front-page/js/common.js
@@ -1,6 +1,4 @@
 $(function () {
-  const anchors = document.querySelectorAll('a[href*="#"]');
-
   $(document).ready(function() {
   $('.slider').slick({
     arrows:false,
@@ -11,25 +9,51 @@ $(function () {
     arrows:false,
     dots:true,  
     slidesToShow:6,
+    responsive:[
+      {
+        breakpoint: 1450,
+        settings: {
+          slidesToShow:5,
+          slidesToScroll:1,
+        }
+      },
+    ]
   });
 });
 
+ const anchors = document.querySelectorAll('a[href*="#"]');
   for (let anchor of anchors) {
     anchor.addEventListener("click", function (event) {
       event.preventDefault();
       const blockID = anchor.getAttribute("href");
     });
   }
-
+  
 });
 
-$(document).on("click", ".box", function () {
-  $(this).addClass("active").siblings().removeClass("active");
+$ (function() {
+    $('#btn__one') .ready (function() {
+        $('.form__tel') .hide();
+        $('.video__form') .hide();
+    })
+    $('.button__tel a') .click(function() {
+        $('.form__tel') .show();
+    })
+    $('.exit-form') .click(function() {
+        $('.form__tel') .hide();
+    })
+    $('.button__tel a') .click(function() {
+        $('.form__tel') .show();
+    })
+    $('.video__item a') .click(function() {
+        $('.video__form') .show();
+    })
+    $('.video__form__exit') .click(function() {
+        $('.video__form') .hide();
+    })
+   
 });
 
-$('.popup-youtube').magnificPopup({ 
-    type: 'iframe' 
- });
 
 
 
